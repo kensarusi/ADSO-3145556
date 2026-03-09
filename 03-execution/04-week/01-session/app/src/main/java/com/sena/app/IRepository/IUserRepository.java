7c258501fea316dff650f2aeb4b5a90e94e6e23c
ken's <kensarusi@gmail.com>
2026-03-09 14:55:06 -0500

    siu

--- 03-execution/04-week/01-session/app/src/main/java/com/sena/app/IRepository/IUserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.sena.app.IRepository;
-
-import com.sena.app.Entity.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface IUserRepository extends JpaRepository<User, Long> {
-}
